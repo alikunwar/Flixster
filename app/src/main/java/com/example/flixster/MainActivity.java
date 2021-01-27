8bf98d7bdd00e93c0f57737c55e3269e9b765084
alikunwar <alisha.kunwar@mavs.uta.edu>
2021-01-27 11:52:59 -0600

    Initial commit

---
+++ app/src/main/java/com/example/flixster/MainActivity.java
@@ -0,0 +1,78 @@
+package com.example.flixster;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+import android.util.Log;
+
+import com.codepath.asynchttpclient.AsyncHttpClient;
+import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler;
+import com.example.flixster.adapters.MovieAdapter;
+import com.example.flixster.models.Movie;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import okhttp3.Headers;
+
+public class MainActivity extends AppCompatActivity {
+
+    List<Movie> movies;
+    public static final String NOW_PLAYING_URL = "https://api.themoviedb.org/3/movie/now_playing?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed";
+    public static final String TAG ="MainActivity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        RecyclerView rvMovies = findViewById(R.id.rvMovies);
+        movies= new ArrayList<>();
+
+        //Creating the adapter
+        MovieAdapter movieAdapter = new MovieAdapter(this,movies);
+
+        //Setting the adapter
+        rvMovies.setAdapter(movieAdapter);
+
+        //Setting a Layout manger on the recycler view
+        rvMovies.setLayoutManager(new LinearLayoutManager(this));
+
+        //Creating instant of AsyncHttpclient
+        AsyncHttpClient client =new AsyncHttpClient();
+
+        //making get request to the movie database url
+        client.get(NOW_PLAYING_URL, new JsonHttpResponseHandler() {
+            @Override
+
+            //i refers to the network status either success or failure
+            //the movie list are stored in an object called json
+            public void onSuccess(int i, Headers headers, JSON json) {
+                Log.d(TAG,  "onSuccess");
+                JSONObject jsonObject = json.jsonObject;
+                try {
+                    JSONArray results = jsonObject.getJSONArray("results");
+                    Log.i(TAG, "Results: " + results.toString());
+                    movies.addAll(Movie.fromJsonArray(results));
+                    movieAdapter.notifyDataSetChanged();
+                    Log.i(TAG, "Movies: " + movies.size());
+                } catch (JSONException e) {
+                    Log.e(TAG, "Json exception",e);
+                    e.printStackTrace();
+                }
+
+            }
+
+            @Override
+            public void onFailure(int i, Headers headers, String s, Throwable throwable) {
+                Log.d(TAG,  "onFailure");
+            }
+        });
+
+    }
+}
